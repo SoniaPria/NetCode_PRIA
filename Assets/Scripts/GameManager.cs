c78f0707fb2c065c53b4b7cfa6311546f1227a7c
SoniaPria <webusable@protonmail.com>
2023-05-23 20:53:47 +0200

    Bug as instancia de player non muda de cor

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,80 +3,70 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-namespace HelloWorld
+public class GameManager : NetworkBehaviour
 {
-    public class GameManager : NetworkBehaviour
+    public static GameManager instance;
+
+    void OnEnable()
     {
-        public static GameManager instance;
-
-        // public List<Material> playerColors;
-
-        float axisLeft, axisRight;
-
-        void OnEnable()
+        if (instance != null && instance != this)
         {
-            if (instance != null && instance != this)
-            {
-                Destroy(this);
-            }
-            instance = this;
+            Destroy(this);
+        }
+        instance = this;
+    }
 
-            axisLeft = -2f;
-            axisRight = 2f;
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+        if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+        {
+            StartButtons();
+        }
+        else
+        {
+            StatusLabels();
+            SubmitNeutralZone();
         }
 
-        void OnGUI()
+        GUILayout.EndArea();
+    }
+
+    static void StartButtons()
+    {
+        if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
+        if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+        if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
+    }
+
+    static void StatusLabels()
+    {
+        var mode = NetworkManager.Singleton.IsHost ?
+            "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
+
+        GUILayout.Label("Transport: " +
+            NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
+        GUILayout.Label("Mode: " + mode);
+    }
+
+    static void SubmitNeutralZone()
+    {
+        if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Todos a Inicio" : "Mover a inicio"))
         {
-            GUILayout.BeginArea(new Rect(10, 10, 300, 300));
-            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+            if (NetworkManager.Singleton.IsServer)
             {
-                StartButtons();
+                foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
+                {
+                    NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid)
+                        .GetComponent<Player>()
+                        .MoveNeutralClientRpc();
+                }
             }
             else
             {
-                StatusLabels();
-                SubmitJump();
-            }
-
-            GUILayout.EndArea();
-        }
-
-        static void StartButtons()
-        {
-            if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
-            if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
-            if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
-        }
-
-        static void StatusLabels()
-        {
-            var mode = NetworkManager.Singleton.IsHost ?
-                "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
-
-            GUILayout.Label("Transport: " +
-                NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
-            GUILayout.Label("Mode: " + mode);
-        }
-
-        static void SubmitJump()
-        {
-            if (GUILayout.Button(NetworkManager.Singleton.IsServer ? "Todos a Inicio" : "Mover a inicio"))
-            {
-                if (NetworkManager.Singleton.IsServer)
-                {
-                    foreach (ulong uid in NetworkManager.Singleton.ConnectedClientsIds)
-                    {
-                        NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(uid)
-                            .GetComponent<Player>()
-                            .MoveNeutralClientRpc();
-                    }
-                }
-                else
-                {
-                    var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
-                    var player = playerObject.GetComponent<Player>();
-                    player.MoveNeutralServerRpc();
-                }
+                var playerObject = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
+                var player = playerObject.GetComponent<Player>();
+                player.MoveNeutralServerRpc();
             }
         }
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,192 +3,143 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-namespace HelloWorld
+public class Player : NetworkBehaviour
 {
-    public class Player : NetworkBehaviour
+    NetworkVariable<int> PlayerColor = new NetworkVariable<int>();
+
+    [SerializeField]
+    List<Material> playerColors;
+
+    MeshRenderer mr;
+
+    void Start()
     {
-        [SerializeField]
-        List<Material> playerColors;
+    }
 
-        MeshRenderer mr;
-
-
-        // [SerializeField]
-        // List<Material> playerColors;
-
-        void Start()
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
         {
+            SetStartPositionServerRpc();
+            PlayerColor.Value = 0;
+        }
+    }
+
+    public void MoveNeutral()
+    {
+        Debug.Log($"{gameObject.name}.MoveNeutral");
+
+        Vector3 tmpPosition = transform.position;
+        tmpPosition.x = Random.Range(-1.5f, 1.5f);
+        transform.position = tmpPosition;
+    }
+
+    [ClientRpc]
+    public void MoveNeutralClientRpc(ClientRpcParams rpcParams = default)
+    {
+        MoveNeutral();
+    }
+
+    [ServerRpc]
+    public void MoveNeutralServerRpc(ServerRpcParams rpcParams = default)
+    {
+        MoveNeutral();
+    }
+
+
+    [ServerRpc]
+    public void SetTeemColorServerRpc(int zone = 0, ServerRpcParams clientRpcParams = default)
+    {
+        if (zone == 1)
+        {
+            mr.material = playerColors[1];
         }
 
-        public override void OnNetworkSpawn()
+        else if (zone == 2)
         {
-            Initialize();
-
-            if (IsOwner)
-            {
-                // Para que non se espaneen no mesmo punto
-                SetStartPositionServerRpc();
-            }
+            mr.material = playerColors[2];
         }
 
-        void Initialize()
+        else
         {
-            // PlayerColor.Value = 0;
-            mr = GetComponent<MeshRenderer>();
+            mr.material = playerColors[2];
+        }
+    }
 
-            // Dev
-            var ngo = GetComponent<NetworkObject>();
-            string uid = ngo.NetworkObjectId.ToString();
 
-            if (ngo.IsOwnedByServer)
-            {
-                gameObject.name = $"HostPlayer_{uid}";
-            }
-            else if (ngo.IsOwner)
-            {
-                gameObject.name = $"LocalPlayer_{uid}";
-            }
-            else
-            {
-                gameObject.name = "Net_Player_" + uid;
-            }
 
-            Debug.Log($"{gameObject.name}.Player");
-            Debug.Log($"\t IsLocalPlayer: {ngo.IsLocalPlayer}");
-            Debug.Log($"\t IsOwner: {ngo.IsOwner}");
-            Debug.Log($"\t IsOwnedByServer: {ngo.IsOwnedByServer}");
-            // --- end Dev
+    [ServerRpc]
+    void SetStartPositionServerRpc(ServerRpcParams rpcParams = default)
+    {
+        // Posición aleatoria no taboleiro
+        // No espazo centra no que o xogador aínda non ten equipo
+        transform.position = new Vector3(Random.Range(-1.5f, 1.5f), 1f, Random.Range(-4.5f, 5f));
+    }
+
+    [ServerRpc]
+    void MoveServerRpc(Vector3 direction, ServerRpcParams rpcParams = default)
+    {
+        // Posición enviada por Input de Player
+        // Transform se propaga en rede sen Network variable
+        transform.position += direction;
+    }
+
+
+
+    void Update()
+    {
+        if (!IsOwner) { return; }
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            MoveServerRpc(Vector3.forward);
         }
 
-        public void MoveNeutral()
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            Debug.Log($"{gameObject.name}.MoveNeutral");
-
-            Vector3 tmpPosition = transform.position;
-            tmpPosition.x = Random.Range(-1.5f, 1.5f);
-            transform.position = tmpPosition;
+            MoveServerRpc(Vector3.right);
         }
 
-        [ClientRpc]
-        public void MoveNeutralClientRpc(ClientRpcParams rpcParams = default)
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            MoveNeutral();
+            MoveServerRpc(Vector3.back);
         }
 
-        [ServerRpc]
-        public void MoveNeutralServerRpc(ServerRpcParams rpcParams = default)
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            MoveNeutral();
+            MoveServerRpc(Vector3.left);
         }
 
-        public void Print()
+        if (mr.material.color != playerColors[PlayerColor.Value].color)
         {
-            Debug.Log($"{gameObject.name}.Print");
-            PrintServerRpc();
-        }
-        // void InitRandomPosition() { SubmitRandomPositionRequestServerRpc(); }
-
-
-        [ClientRpc]
-        public void SetTeemColorClientRpc(int zone = 0, ClientRpcParams clientRpcParams = default)
-        {
-            if (zone == 1)
-            {
-                mr.material = playerColors[1];
-            }
-
-            else if (zone == 2)
-            {
-                mr.material = playerColors[2];
-            }
-
-            else
-            {
-                mr.material = playerColors[2];
-            }
+            mr.material = playerColors[PlayerColor.Value];
         }
 
-
-
-        [ServerRpc]
-        void PrintServerRpc()
+        if (transform.position.x < -1.5f)
         {
-            Debug.Log($"{gameObject.name}.PrintServerRpc");
+            PlayerColor.Value = 1;
+            mr.material = playerColors[1];
+            // SetTeemColorServerRpc(1);
+
+            Debug.Log($"{gameObject.name} X = {transform.position.x} Equipo Vermello");
         }
 
-
-        [ServerRpc]
-        void SetStartPositionServerRpc(ServerRpcParams rpcParams = default)
+        else if (transform.position.x > 1.5f)
         {
-            // Posición aleatoria no taboleiro
-            // No espazo centra no que o xogador aínda non ten equipo
-            transform.position = new Vector3(Random.Range(-1.5f, 1.5f), 1f, Random.Range(-4.5f, 5f));
+            PlayerColor.Value = 2;
+            mr.material = playerColors[1];
+            // SetTeemColorServerRpc(2);
+
+            Debug.Log($"{gameObject.name} X = {transform.position.x} Equipo Azul");
         }
 
-        [ServerRpc]
-        void MoveServerRpc(Vector3 direction, ServerRpcParams rpcParams = default)
+        else
         {
-            // Posición enviada por Input de Player
-            // Transform se propaga en rede sen Network variable
-            transform.position += direction;
-        }
+            PlayerColor.Value = 0;
+            mr.material = playerColors[1];
+            // SetTeemColorServerRpc(0);
 
-        void Update()
-        {
-            if (!IsOwner) { return; }
-
-            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                // Debug.Log($"{gameObject.name}.Player.Update");
-                // Debug.Log($"\t Input W | Input Up arrow");
-
-                MoveServerRpc(Vector3.forward);
-            }
-
-            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                // Debug.Log($"{gameObject.name}.Player.Update");
-                // Debug.Log($"\t Input D | Input Right arrow");
-
-                MoveServerRpc(Vector3.right);
-            }
-
-            if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                // Debug.Log($"{gameObject.name}.Player.Update");
-                // Debug.Log($"\t Input S | Input Down arrow");
-
-                MoveServerRpc(Vector3.back);
-            }
-
-            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                // Debug.Log($"{gameObject.name}.Player.Update");
-                // Debug.Log($"\t Input A | Input Left arrow");
-
-                MoveServerRpc(Vector3.left);
-            }
-            // if (mr.material.color != GameManager.instance.playerColors[PlayerColor.Value].color)
-            // {
-            //     // Debug.Log($"{gameObject.name}.HelloWorldPlayer.Update Material");
-
-            //     mr.material = GameManager.instance.playerColors[PlayerColor.Value];
-            // }
-
-            if (transform.position.x < -1.5f)
-            {
-                Debug.Log($"{gameObject.name} X = {transform.position.x} Equipo Vermello");
-            }
-
-            else if (transform.position.x > 1.5f)
-            {
-                Debug.Log($"{gameObject.name} X = {transform.position.x} Equipo Azul");
-            }
-
-            else
-            {
-                Debug.Log($"{gameObject.name} X = {transform.position.x} Equipo Neutro");
-            }
+            Debug.Log($"{gameObject.name} X = {transform.position.x} Equipo Neutro");
         }
     }
 }
